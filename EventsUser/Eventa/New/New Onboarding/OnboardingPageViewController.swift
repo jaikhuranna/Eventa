610bd299c97c8002b577dd242789b931c584254b
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-02-02 15:13:19 +0530

    update

--- EventsUser/Eventa/New/New Onboarding/OnboardingPageViewController.swift
+++ EventsUser/Eventa/New/New Onboarding/OnboardingPageViewController.swift
@@ -8,20 +8,35 @@
 import UIKit
 
 class OnboardingPageViewController: UIPageViewController, UIPageViewControllerDataSource{
-    
+
+    var pages: [UIViewController] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataSource = self;
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+               let On1 = storyboard.instantiateViewController(withIdentifier: "OnboardingPage1")
+               let On2 = storyboard.instantiateViewController(withIdentifier: "OnboardingPage2")
+               
+        pages = [On1, On2]
+
+               if let firstPage = pages.first {
+                   setViewControllers([firstPage], direction: .forward, animated: true)
+               }
 
     }
     
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        guard let index = pages.firstIndex(of: viewController), index > 0 else { return nil }
+        return pages[index - 1]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        guard let index = pages.firstIndex(of: viewController), index < pages.count - 1 else { return nil }
+        return pages[index + 1]
     }
 
 }
