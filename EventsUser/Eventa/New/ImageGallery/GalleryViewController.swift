312e098942ff1fa10586f8ebb0f3a6263bfbf1cc
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2025-02-06 03:15:26 +0530

    fix git issue - add gallery

--- EventsUser/Eventa/New/ImageGallery/GalleryViewController.swift
+++ EventsUser/Eventa/New/ImageGallery/GalleryViewController.swift
@@ -16,22 +16,15 @@ class GalleryViewController: UIViewController, UICollectionViewDataSource, UICol
     
     var images: [ImageModel] = [] // Array to store image data
     var eventID: String = "23156AAB-DB89-478C-804B-22DF94A0B4AF" // Pass this dynamically
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        collectionView.setCollectionViewLayout(generateLayout(), animated: true)
         // Explicitly setting flow layout
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumInteritemSpacing = 8
-        layout.minimumLineSpacing = 8
-        collectionView.collectionViewLayout = layout
-        
         fetchImages()
-        
-        //long-press to save (trying to implement)
         
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
         collectionView.addGestureRecognizer(longPressGesture)
@@ -201,15 +194,6 @@ extension GalleryViewController: UIImagePickerControllerDelegate, UINavigationCo
 // sizeForItemAt adjustment
 
 
-extension GalleryViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRow: CGFloat = 3 // Adjust for desired grid size
-        let padding: CGFloat = 8
-        let totalPadding = (itemsPerRow - 1) * padding
-        let itemWidth = (collectionView.bounds.width - totalPadding) / itemsPerRow
-        return CGSize(width: itemWidth, height: itemWidth) // Make it a square
-    }
-
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 8 // Adjust spacing between columns
     }
@@ -217,7 +201,28 @@ extension GalleryViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 8 // Adjust spacing between rows
     }
+
+func generateLayout() -> UICollectionViewLayout {
+    return UICollectionViewCompositionalLayout { (sectionIndex, env) -> NSCollectionLayoutSection? in
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0/3.0),
+                                              heightDimension: .fractionalWidth(1.0/3.0)) // Square items
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .fractionalWidth(1.0/3.0)) // Match item height
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       repeatingSubitem: item, count: 3)
+        group.interItemSpacing = .fixed(2) // Spacing between items
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 2 // Spacing between rows
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 2, bottom: 10, trailing: 2)
+
+        return section
+    }
 }
+
 
 
 // extension to load image properly, (has sm to do with saving image)
@@ -234,84 +239,3 @@ extension UIImageView {
         }
     }
 }
-//import UIKit
-//
-//private let reuseIdentifier = "Cell"
-//
-//class GalleryViewController: UICollectionViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Uncomment the following line to preserve selection between presentations
-//        // self.clearsSelectionOnViewWillAppear = false
-//
-//        // Register cell classes
-//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using [segue destinationViewController].
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//    // MARK: UICollectionViewDataSource
-//
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of items
-//        return 0
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-//    
-//        // Configure the cell
-//    
-//        return cell
-//    }
-//
-//    // MARK: UICollectionViewDelegate
-//
-//    /*
-//    // Uncomment this method to specify if the specified item should be highlighted during tracking
-//    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//    */
-//
-//    /*
-//    // Uncomment this method to specify if the specified item should be selected
-//    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//    */
-//
-//    /*
-//    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-//    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-//        return false
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-//        return false
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-//    
-//    }
-//    */
-//
-//}
